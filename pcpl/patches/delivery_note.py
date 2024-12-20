d4695b4d5af1100b9c7cc8603d46dd4f13378dac
krunal-sigzen <krunal.p@sigzen.com>
2024-12-20 09:59:14 +0000

    Feat: Update Cancel DN (Advance Received) Field

---
+++ pcpl/patches/delivery_note.py
@@ -0,0 +1,9 @@
+import frappe
+
+
+def execute():
+    delivery_notes = frappe.get_list("Delivery Note", filters={"status": "Canceled"}, fields=["name"])
+    for doc in delivery_notes:
+         frappe.db.set_value("Delivery Note", doc.name, "custom_advance_received", 0.0)
+    frappe.db.commit() # nosemgrep
+        
