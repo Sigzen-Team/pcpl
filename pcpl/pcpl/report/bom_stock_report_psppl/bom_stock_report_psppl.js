a617c2ed822526d5e480d2f5dea32c0ef875d252
Hevin-sigzen <hevin.b@sigzen.com>
2025-03-11 18:56:00 +0530

    Staging

---
+++ pcpl/pcpl/report/bom_stock_report_psppl/bom_stock_report_psppl.js
@@ -0,0 +1,35 @@
+// Copyright (c) 2025, STPL and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["BOM Stock Report PSPPL"] = {
+	"filters": [
+		{
+			"fieldname":"name",
+			"label":__("BOM"),
+			"fieldtype":"Link",
+			"options":"BOM",
+			"reqd": 1,
+			"get_query": () =>{
+				return {
+					filters: { "is_active": 1 , "is_default":1}
+				}
+			}
+		},
+		{
+			"fieldname":"qty_to_produce",
+			"label":__("Qty to Produce"),
+			"fieldtype":"Float",
+			"reqd": 1
+		}
+	],
+	"formatter": function (value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		if (column.fieldname == "Short Qty" && data && data['Short Qty'] > 0) {
+			value =`<span style='color:green; font-weight: bold;'>${value}</span>`;
+		}
+		else if (in_list(["Short Qty", "Item Code"],column.fieldname) && data && data['Short Qty'] < 0) {
+			value =`<style>span>a{color : red;}</style><span style='color:red; font-weight: bold;'>${value}</span>`;
+		}
+		return value;
+	},
+};
